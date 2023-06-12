e897a9f5f3fe087a95a786f957fcd327a4d0b24b
MetroplexHeeds <120651222+MetroplexHeeds@users.noreply.github.com>
2023-06-12 19:57:18 +0530

    Update victim.py

--- victim.py
+++ victim.py
@@ -14,13 +14,13 @@ async def on_message(message):
         await message.channel.send("https://tenor.com/view/kinggwilliamss-licking-lips-gif-24417239")
     if "high" in dudu:
         await message.channel.send('https://tenor.com/view/i-show-speed-speed-shake-now-suck-that-sucking-gif-24039341')
-    if message.author.id == message.guild.owner.id:
-        if '!exile' == dudu[0]:
-            user = message.mentions[0]
-            role = discord.utils.get(message.guild.roles, name="Verified")
-            await user.remove_roles(role)
-            await message.channel.send("Exiled")
-            await message.channel.send("https://tenor.com/view/metroplex-transformers-war-for-cybertron-gif-18216764")
+    if '!exile' == dudu[0]:
+        user = message.mentions[0]
+        role = discord.utils.get(message.guild.roles, name="Verified")
+        await user.remove_roles(role)
+        await message.channel.send("Exiled")
+        await message.channel.send("https://tenor.com/view/metroplex-transformers-war-for-cybertron-gif-18216764")
+            
 
 
 @bot.command(name="add")
@@ -53,6 +53,20 @@ async def clear(ctx, amount: int):
         await ctx.channel.purge(limit=amount)
         await ctx.channel.send("Eradicated harams")
         await ctx.channel.send("https://tenor.com/view/metroplex-transformers-war-for-cybertron-gif-18216764")
+async def timetable(ctx, day):
+    if day=="mon":
+        await ctx.channel.send("CS Phy Eng Chem PT Chem Meth Art Phy")
+    elif day=="tue":
+        await ctx.channel.send("Meth AI Eng CS Chem Physics Meth Chemlab")
+    elif day=="wed":
+        await ctx.channel.send("Meth Physics Meth PT Physics Chem English CSlab")
+    elif day=="thu":
+        await ctx.channel.send("CS AI Chem Eng Eng Meth GC CS Phy")
+    elif day=="fri":
+        await ctx.channel.send("CS Meth Eng AI AI Chem Math Phylab")
+    else:
+        await ctx.channel.send("Dei moodheri invalid day")
+        await ctx.channel.send("https://media.discordapp.net/attachments/923098562701692959/1117822314164277418/kys.png?width=523&height=339")
     
 bot.run("MTA1MTg1OTAzNDE5OTgxODI2MA.G-97E8.w1ZIKxl9581NLK_y9LZYyi7CVLOsF7ZEOQ8Y70")
 
