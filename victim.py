92fbbc81c887bb9a2da9dba26b48bd01972a69dd
MetroplexHeeds <120651222+MetroplexHeeds@users.noreply.github.com>
2023-08-31 19:19:45 +0530

    Update victim.py

--- victim.py
+++ victim.py
@@ -1,5 +1,4 @@
 import discord
-import time
 from discord.ext import commands
 bot = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 
@@ -9,18 +8,19 @@ tasks = {}
 async def on_message(message):
     await bot.process_commands(message)
     dudu = message.content.lower().split()
-    if "metroplex" in dudu and message.author.bot == False:
-        await message.channel.send('Metroplex heeds the call of the last prime!')
-        await message.channel.send("https://tenor.com/view/metroplex-transformers-war-for-cybertron-gif-18216764")
-    if "du" in dudu:
-        await message.channel.send("https://tenor.com/view/kinggwilliamss-licking-lips-gif-24417239")
-    if "high" in dudu:
-        await message.channel.send('https://tenor.com/view/i-show-speed-speed-shake-now-suck-that-sucking-gif-24039341')
-    if "lalo" in dudu:
-        await message.channel.send("https://media.discordapp.net/attachments/994823243628286026/1103230139833253898/you_can_call_me_lalo.gif")
-    if "roberto" in dudu:
-        await message.channel.send("https://media.discordapp.net/attachments/1001823457861971979/1087991719791968266/ezgif.com-optimize_2.gif")
-        
+    try:
+        if "metroplex" in dudu and message.author.bot == False:
+            await message.channel.send('Metroplex heeds the call of the last prime!')
+            await message.channel.send("https://tenor.com/view/metroplex-transformers-war-for-cybertron-gif-18216764")
+        if "du" in dudu:
+            await message.channel.send("https://tenor.com/view/kinggwilliamss-licking-lips-gif-24417239")
+        if "high" in dudu:
+            await message.channel.send('https://tenor.com/view/i-show-speed-speed-shake-now-suck-that-sucking-gif-24039341')
+        if "lalo" in dudu:
+            await message.channel.send("https://media.discordapp.net/attachments/994823243628286026/1103230139833253898/you_can_call_me_lalo.gif")
+        if "roberto" in dudu:
+            await message.channel.send("https://media.discordapp.net/attachments/1001823457861971979/1087991719791968266/ezgif.com-optimize_2.gif")
+    except:
 
 @bot.command(name="add")
 async def add_task(ctx, task, due_date):
@@ -83,19 +83,21 @@ async def unexile(ctx, target):
     
 @bot.command(name="timetable")        
 async def timetable(ctx, day):
-    if day=="mon":
-        await ctx.channel.send("CS Phy Eng Chem PT Chem Meth Art Phy")
-    elif day=="tue":
-        await ctx.channel.send("Meth AI Eng CS Chem Physics Meth Chemlab")
-    elif day=="wed":
-        await ctx.channel.send("Meth Physics Meth PT Physics Chem English CSlab")
-    elif day=="thu":
-        await ctx.channel.send("CS AI Chem Eng Eng Meth GC CS Phy")
-    elif day=="fri":
-        await ctx.channel.send("CS Meth Eng AI AI Chem Math Phylab")
-    else:
-        await ctx.channel.send("Dei moodheri invalid day")
-        await ctx.channel.send("https://media.discordapp.net/attachments/923098562701692959/1117822314164277418/kys.png?width=523&height=339")
+    try:
+        if day.lower()=="mon":
+            await ctx.channel.send("CS Phy Eng Chem PT Chem Meth Art Phy")
+        elif day.lower()=="tue":
+            await ctx.channel.send("Meth AI Eng CS Chem Physics Meth Chemlab")
+        elif day.lower()=="wed":
+            await ctx.channel.send("Meth Physics Meth PT Physics Chem English CSlab")
+        elif day.lower()=="thu":
+            await ctx.channel.send("CS AI Chem Eng Eng Meth GC CS Phy")
+        elif day.lower()=="fri":
+            await ctx.channel.send("CS Meth Eng AI AI Chem Math Phylab")
+        else:
+            await ctx.channel.send("Dei moodheri invalid day")
+            await ctx.channel.send("https://media.discordapp.net/attachments/923098562701692959/1117822314164277418/kys.png?width=523&height=339")
+    except:
 bot.run("MTA1MTg1OTAzNDE5OTgxODI2MA.GqFMWq.P8Am4tM5FOpQn2RD--KX9ERCczPAacPCO0sqFE")
 
 
